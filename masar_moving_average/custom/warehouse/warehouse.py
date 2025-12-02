ab416ab34cb618cd642c24ee7e6a1a1aac276109
karamakcsc <karamakcsc@hotmail.com>
2025-12-02 11:48:47 +0300

    update

--- masar_moving_average/custom/warehouse/warehouse.py
+++ masar_moving_average/custom/warehouse/warehouse.py
@@ -4,6 +4,7 @@ def validate(self , method):
     remove_warehouse_from_other_cost_zones(self)
     if not self.is_new():
         remove_warehouse_from_other_cost_zones(self)
+    add_warehouse_to_cost_zone(self)
     
 def on_trash(self , method):
     remove_warehouse_from_other_cost_zones_on_delete(self)
@@ -25,9 +26,9 @@ def add_warehouse_to_cost_zone(self):
 def remove_warehouse_from_other_cost_zones(self):
     wh_exist = frappe.db.sql("""
         SELECT parent FROM `tabCost Zone Details`
-        WHERE warehouse = %s AND parent != %s
-    """, (self.name, self.custom_cost_zone), as_dict=1)
-
+        WHERE warehouse = %s
+    """, (self.name,), as_dict=1)
+    
     for wh in wh_exist:
         cost_zone = frappe.get_doc("Cost Zone", wh.parent)
         cost_zone.warehouses = [w for w in cost_zone.warehouses if w.warehouse != self.name]
